7576d75ff4256accc4937ea6c552f35e463ff7a3
prithviBytes <prithvisuvarna56@gmail.com>
2021-08-30 19:48:16 +0530

    add: jwt interceptor

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -3,15 +3,6 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-const storageData = JSON.parse(localStorage.getItem("users") || "{}");
-const token = storageData['token'] ? storageData['token'] : null;
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    token: token ? token : ""
-  })
-}
-
 @Injectable({
   providedIn: 'root'
 })
@@ -21,10 +12,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + "users", httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + "users");
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + `user/${username}`, httpOptions);
+    return this.http.get<Member>(this.baseUrl + `user/${username}`);
   }
 }
