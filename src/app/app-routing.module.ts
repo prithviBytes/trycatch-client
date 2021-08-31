c60b50267b33f7186bd0884a32376458fd7a9ad4
prithviBytes <prithvisuvarna56@gmail.com>
2021-08-31 11:57:15 +0530

    add: member edit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,27 +6,29 @@ import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_gaurds/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   {
     path: "",
     runGuardsAndResolvers: "always",
     canActivate: [AuthGuard],
     children: [
-      { path: "members", component: MemberListComponent, canActivate: [AuthGuard]},
-      { path: "members/:username", component: MemberDetailComponent},
-      { path: "lists", component: ListsComponent},
-      { path: "messages", component: MessagesComponent}
+      { path: "members", component: MemberListComponent },
+      { path: "members/:username", component: MemberDetailComponent },
+      { path: "member/edit", component: MemberEditComponent },
+      { path: "lists", component: ListsComponent },
+      { path: "messages", component: MessagesComponent }
     ]
   },
-  {path: "not-found", component: NotFoundComponent},
-  {path: "server-error", component: ServerErrorComponent},
-  {path: "errors", component: TestErrorComponent},
-  {path: "**",component: NotFoundComponent,pathMatch: "full"}
+  { path: "not-found", component: NotFoundComponent },
+  { path: "server-error", component: ServerErrorComponent },
+  { path: "errors", component: TestErrorComponent },
+  { path: "**", component: NotFoundComponent, pathMatch: "full" }
 ];
 
 @NgModule({
