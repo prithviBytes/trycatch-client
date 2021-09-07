f2e5299078aa18743aed3bbe08ab6381f345eb8a
prithviBytes <prithvisuvarna56@gmail.com>
2021-09-07 20:57:59 +0530

    add: extend client side resister component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -11,10 +12,15 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
-  model: any = {}
   registerForm!: FormGroup;
   maxDate!: Date;
-  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) { }
+  validationErrors: string[] = [];
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService,
+    private fb: FormBuilder,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -47,10 +53,9 @@ export class RegisterComponent implements OnInit {
   }
 
   register(){
-    console.log(this.registerForm);
-    // this.accountService.register(this.model).subscribe(response => {
-    //   this.cancel();
-    // }, error => this.toastr.error(error.error))
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl("/members");
+    }, error => this.validationErrors = error)
   }
   cancel(){
     this.cancelRegister.emit(false);
